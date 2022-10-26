a63b713c4d3911b01375d2511fac95aec4bb4bf3
kajuSarkar <kajusarkar@gmail.comm>
2022-10-26 10:45:02 -0500

    testing middleware  for /user/login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,27 +2,28 @@ const { application } = require('express');
 const db = require('./models/capableHumanModels');
 const path = require('path');
 
-const userController = {};
 
-app.post ('/login', async(req, res) => {
- try {
-  const {username, password} = req.body;
-  const hash = await bcrypt.hash(password, 10);
-  await db('users').insert({username, password: hash});
-  res.status(201).json({message: 'its Gucci'});
- } catch(e){
-   console.log (e)
-   res.status(500).json({message: 'its not Gucci'});
- }
- });
- // from users table select first row where username = username
- app.post('/login', async (req, res) => {
+const userController = {};
+userController.loginUser = async(req,res,next) =>  {
+  try {
+   const {username, password} = req.body;
+   const hash = await bcrypt.hash(password, 10);
+   await db('users').insert({username, password: hash});
+   res.status(201).json({message: 'its Gucci'});
+  } catch(e){
+    console.log (e)
+    res.status(500).json({message: 'its not Gucci'});
+  }
+  };
+
+ 
+ app.post('/signup', async (req, res) => {
   try {
    const user = await db('users').first('*').where({username: req.body.username});
    if (user){
     const userGucci = await bcrypt.compare(req.body.password, user.password);
     if (valid){
-     res.status(200).json({message: 'its Gucci'});
+     return next ()
     } else {
      res.status(401).json({message: 'its not Gucci'});
     }
@@ -31,10 +32,13 @@ app.post ('/login', async(req, res) => {
    console.log (e)
    res.status(500).json({message: ' User AWOL ...its not Gucci'});
   } 
- }
+ });
  
-
+const Authcontroller = {};
+Authcontroller.verifyUser = async (req, res, next) => {}
 
 
 
 module.exports = userController;
+
+//`SELECT * FROM test.users WHERE password=‘${hash}’`
